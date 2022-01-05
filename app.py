dde63d34276611b8eaace7e2cdcafd7a4701769a
diegogonza112 <diego.gonzalezdelgado@mail.utoronto.ca>
2022-01-05 13:26:30 -0500

    first steps

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Welcome to index page"
+
+
+@app.route("/hi/")
+def who():
+    return "who are you"
+
+
+@app.route("/hi/<username>")
+def greet(username):
+    return f"Hi there {username}"
+
