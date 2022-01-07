b7ba1adf300275b341ddaf3122acbfd7de570031
diegogonza112 <diego.gonzalezdelgado@mail.utoronto.ca>
2022-01-07 11:06:53 -0500

    Adding more functions and fixing bugs
    Still having trouble downloading .csv file

--- user_crud/app.py
+++ user_crud/app.py
@@ -1,5 +1,7 @@
+import os
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, current_app, render_template, request, redirect, \
+    send_file, send_from_directory
 from werkzeug.exceptions import abort
 
 from models import db, ProductModel
@@ -22,22 +24,34 @@ def home():
         return render_template('home.html')
 
     if request.method == 'POST':
-        product_id = request.form['product_id']
-        product_name = request.form['product_name']
-        quant = request.form['quantity']
-        category = request.form['product_category']
-        product = ProductModel(product_id=product_id, product_name=product_name,
-                               quantity=quant,
-                               product_category=category)
-        db.session.add(product)
-        db.session.commit()
-        return redirect('/data')
+        if request.form['btn_identifier'] == 'newInput':
+            product_id = request.form['product_id']
+            product_name = request.form['product_name']
+            quant = request.form['quantity']
+            category = request.form['product_category']
+            product = ProductModel(product_id=product_id,
+                                   product_name=product_name,
+                                   quantity=quant,
+                                   product_category=category)
+            csv_row = f"{product_name}, {quant}, {category}, {product_id}"
+            with open('product_info.csv', 'a') as fd:
+                fd.write(csv_row)
+            db.session.add(product)
+            db.session.commit()
+            return redirect('/data')
+        if request.form['btn_identifier'] == 'showData':
+            return redirect('/data')
+        if request.form['btn_identifier'] == 'getCSV':
+            return redirect('/download')
 
 
-@app.route('/data')
+@app.route('/data', methods=['GET', 'POST'])
 def RetrieveList():
-    products = ProductModel.query.all()
-    return render_template('datalist.html', products=products)
+    if request.method == 'GET':
+        products = ProductModel.query.all()
+        return render_template('datalist.html', products=products)
+    if request.method == 'POST':
+        return redirect('/')
 
 
 @app.route('/data/<int:id>')
@@ -46,6 +60,14 @@ def RetrieveProduct(id_):
     if product:
         return render_template('data.html', product=product)
     return f"Product with id ={id_} Doesnt exist"
+
+
+@app.route('/uploads/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    # Appending app path to upload folder path within app root folder
+    uploads = os.path.join(current_app.root_path, app.config['UPLOAD_FOLDER'])
+    # Returning file from appended path
+    return send_from_directory(directory=uploads, filename=filename)
 
 
 @app.route('/data/<int:id>/update', methods=['GET', 'POST'])
--- user_crud/models.py
+++ user_crud/models.py
@@ -19,5 +19,5 @@ class ProductModel(db.Model):
         self.product_category = product_category
 
     def __repr__(self):
-        return f"{self.product_name} | id: {self.product_id} | " \
-               f"Quantity: {self.quantity} | Category: {self.product_category}"
+        return f"{self.product_name} | {self.product_id} | " \
+               f"{self.quantity} | {self.product_category}"
